b97c7ae88cce2aeb04960bf86bab60420e95c9e1
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2025-03-11 19:04:54 +0530

    Implement the MontisoriCP - 4

--- java-concurrency-model/06-inter-thread-communication/src/main/java/lk/ijse/dep13/interthreadcommunication/ReleaseConnectionServlet.java
+++ java-concurrency-model/06-inter-thread-communication/src/main/java/lk/ijse/dep13/interthreadcommunication/ReleaseConnectionServlet.java
@@ -5,8 +5,10 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lk.ijse.dep13.interthreadcommunication.db.MontisoriCP;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -16,26 +18,36 @@ public class ReleaseConnectionServlet extends HttpServlet {
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         if (req.getPathInfo() == null || req.getPathInfo().equals("/")) {
-            releaseAllConnections();
-        }else{
+            releaseAllConnections(resp);
+        } else {
             Matcher matcher = Pattern.compile("^/(?<id>[^/]+)/?(?<rest>.*)$").matcher(req.getPathInfo());
-            if (matcher.find()){
+            if (matcher.find()) {
                 String id = matcher.group("id");
                 String rest = matcher.group("rest");
-                if (rest.isBlank()){
-                    releaseConnection(id);
-                }else{
+                if (rest.isBlank()) {
+                    releaseConnection(id, resp);
+                } else {
                     resp.sendError(404);
                 }
             }
         }
     }
 
-    private void releaseConnection(String id) {
-        System.out.printf("Release Connection(%s)%n", id);
+    private void releaseConnection(String id, HttpServletResponse resp) throws IOException {
+        MontisoriCP connectionPool = (MontisoriCP) getServletContext().getAttribute("datasource");
+        connectionPool.releaseConnection(Integer.valueOf(id.trim()));
+
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.println("<h1>Connection id: " + id + " released</h1>");
     }
 
-    private void releaseAllConnections(){
-        System.out.println("Release All Connections");
+    private void releaseAllConnections(HttpServletResponse resp) throws IOException {
+        MontisoriCP connectionPool = (MontisoriCP) getServletContext().getAttribute("datasource");
+        connectionPool.releaseAllConnections();
+
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.println("<h1>All connections released</h1>");
     }
 }
